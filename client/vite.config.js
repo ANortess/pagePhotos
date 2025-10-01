330dcbcf0fb410ef7b2815830b824543a5782f02
ANortess <nortes.drh@gmail.com>
2025-10-01 18:59:54 +0200

    Inicializate

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Cuando el frontend pida /api/X, Vite lo redirigirá a http://localhost:3001/api/X
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001', // La dirección de tu servidor Express
+        changeOrigin: true,
+        secure: false, // Puedes ponerlo a true si usas HTTPS en el backend
+      },
+    },
+    // Añadir el puerto si no está configurado (por defecto 5173)
+    port: 5173,
+  },
 })
