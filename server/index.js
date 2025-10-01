6b50eb778fdfe8e793a8103e45cb98570e609992
ANortess <nortes.drh@gmail.com>
2025-10-01 20:06:01 +0200

    Inicializate

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const dbConfig = {
     host: process.env.MYSQL_HOST || 'localhost',
     user: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASSWORD || 'password', // Cambia 'password' si usas root sin password localmente
-    database: process.env.MYSQL_DATABASE || 'auth_db',
+    database: process.env.MYSQL_DATABASE || 'Usuarios',
     port: process.env.MYSQL_PORT || 3306,
 };
 
